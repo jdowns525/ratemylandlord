b9374f959317f3fde8b011a7e256b0be5ca3931c
jdowns525 <downsjosseph@gmail.com>
2023-03-19 15:38:05 +0000

    commit

--- app/controllers/landlords_controller.rb
+++ app/controllers/landlords_controller.rb
@@ -27,8 +27,7 @@ class LandlordsController < ApplicationController
     the_landlord.latitude = params.fetch("query_latitude")
     the_landlord.longitude = params.fetch("query_longitude")
     the_landlord.stars = params.fetch("query_stars")
-    the_landlord.review_count = params.fetch("query_review_count")
-    the_landlord.reviews_count = params.fetch("query_reviews_count")
+    
 
     if the_landlord.valid?
       the_landlord.save
