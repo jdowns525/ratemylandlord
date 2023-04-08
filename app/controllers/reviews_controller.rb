67843984b6c2c2db56c0c7476c46042826e10cf2
Josseph Downs <downsjosseph@gmail.com>
2023-04-08 03:11:46 +0000

    commit changes to review controller.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -39,23 +39,29 @@ class ReviewsController < ApplicationController
   def update
     the_id = params.fetch("path_id")
     the_review = Review.where({ :id => the_id }).at(0)
-
+  
     the_review.stars = params.fetch("query_stars")
     the_review.date_occupancy = params.fetch("query_date_occupancy")
     the_review.text = params.fetch("query_text")
     the_review.useful = params.fetch("query_useful")
-    the_review.cool = params.fetch("query_cool")
     the_review.landlord_id = params.fetch("query_landlord_id")
     the_review.user_id = params.fetch("query_user_id")
     the_review.city = params.fetch("query_city")
-
-    if the_review.valid?
-      the_review.save
-      redirect_to("/reviews/#{the_review.id}", { :notice => "Review updated successfully."} )
+  
+    # Check if any changes were made
+    if the_review.changed?
+      # Add additional validation checks
+      if the_review.valid?
+        the_review.save
+        redirect_to("/reviews/#{the_review.id}", { :notice => "Review updated successfully."} )
+      else
+        redirect_to("/reviews/#{the_review.id}", { :alert => the_review.errors.full_messages.to_sentence })
+      end
     else
-      redirect_to("/reviews/#{the_review.id}", { :alert => the_review.errors.full_messages.to_sentence })
+      redirect_to("/reviews/#{the_review.id}", )
     end
   end
+  
 
   def destroy
     the_id = params.fetch("path_id")
