4556a28e0a2c8862f20d4eb11f41695546892d81
Josseph Downs <downsjosseph@gmail.com>
2023-04-12 15:11:21 +0000

    added pagination

--- app/controllers/landlords_controller.rb
+++ app/controllers/landlords_controller.rb
@@ -5,6 +5,11 @@ class LandlordsController < ApplicationController
 
     @list_of_landlords = matching_landlords.order({ :created_at => :desc })
 
+
+
+
+    @landlords = @list_of_landlords.paginate(page: params[:page], per_page: 5)
+
     render({ :template => "landlords/index.html.erb" })
   end
 
