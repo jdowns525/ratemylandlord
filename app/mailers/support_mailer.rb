76bfb3402f06e76474f23844995efedbffb43c13
Josseph Downs <downsjosseph@gmail.com>
2023-04-20 01:59:20 +0000

    support_mailer

--- app/mailers/support_mailer.rb
+++ app/mailers/support_mailer.rb
@@ -2,6 +2,7 @@ class SupportMailer < ApplicationMailer
   default from: 'noreply@example.com'
 
   def support_request_email(support_request)
+    puts "Sending support request email..."
     @support_request = support_request
     mail(to: 'downsjosseph@gmail.com', subject: 'New Support Request')
   end
