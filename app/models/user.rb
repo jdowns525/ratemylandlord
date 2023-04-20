572163115ff5728fa6db19580f5072c94ae5227d
Josseph Downs <downsjosseph@gmail.com>
2023-04-20 17:42:11 +0000

    Revert "commit"
    This reverts commit 8ee2a54ff4a183fccfda21c3a88a3f9204316698.

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,6 @@ class User < ApplicationRecord
   validates :email, :uniqueness => { :case_sensitive => false }
   validates :email, :presence => true
   has_secure_password
-  has_many :landlords
+
   has_many(:reviews, { :class_name => "Review", :foreign_key => "user_id", :dependent => :destroy })
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_20_162453) do
+ActiveRecord::Schema.define(version: 2023_04_20_030605) do
 
   create_table "categories", force: :cascade do |t|
     t.integer "landlord_id"
@@ -34,8 +34,6 @@ ActiveRecord::Schema.define(version: 2023_04_20_162453) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "city"
     t.text "caption"
-    t.integer "user_id"
-    t.index ["user_id"], name: "index_landlords_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -75,5 +73,4 @@ ActiveRecord::Schema.define(version: 2023_04_20_162453) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "landlords", "users"
 end
