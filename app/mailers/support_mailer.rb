12717a154a627ac9d40e7791b69d871752a26816
Josseph Downs <downsjosseph@gmail.com>
2023-04-20 17:03:49 +0000

    commit new mail_request email

--- app/mailers/support_mailer.rb
+++ app/mailers/support_mailer.rb
@@ -4,6 +4,6 @@ class SupportMailer < ApplicationMailer
   def support_request_email(support_request)
     puts "Sending support request email..."
     @support_request = support_request
-    mail(to: 'downsjosseph@gmail.com', subject: 'New Support Request')
+    mail(to: 'mylandlordratings@gmail.com', subject: 'New Support Request')
   end
 end
