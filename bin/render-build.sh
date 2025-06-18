595718326e3028b9c352c695adfc1219ff216c80
Josseph Downs <downsjosseph@gmail.com>
2025-06-18 17:20:48 +0000

    Fix: Force Nokogiri to compile from source on Render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,9 @@
 # exit on error
 set -o errexit
 
+# Force Nokogiri to build from source to avoid GLIBC issues on Render
+bundle config set force_ruby_platform true
+
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
